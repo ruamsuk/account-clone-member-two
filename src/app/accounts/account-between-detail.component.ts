e02c3a9db8037266bebe95a47c9c2a2ccec262d1
ruamsuk <ruamsuk_w@hotmail.com>
2025-03-30 14:05:49 +0700

    Commit II

--- src/app/accounts/account-between-detail.component.ts
+++ src/app/accounts/account-between-detail.component.ts
@@ -4,6 +4,7 @@ import { ConfirmationService } from 'primeng/api';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { catchError, debounceTime, distinctUntilChanged, Observable, of, switchMap, take, tap, throwError } from 'rxjs';
 import { Account } from '../models/account.model';
+import { ThaiDatePipe } from '../pipe/thai-date.pipe';
 import { AccountsService } from '../services/accounts.service';
 import { AuthService } from '../services/auth.service';
 import { ToastService } from '../services/toast.service';
@@ -12,7 +13,8 @@ import { SharedModule } from '../shared/shared.module';
 @Component({
   selector: 'app-account-between-detail',
   imports: [
-    SharedModule
+    SharedModule,
+    ThaiDatePipe
   ],
   template: `
     @if (loading()) {
@@ -22,30 +24,138 @@ import { SharedModule } from '../shared/shared.module';
     }
     <div class="flex flex-wrap p-fluid justify-center items-center mt-2">
       <p-card>
-        <div class="text-center font-thasadith text-base md:text-2xl -mt-3 mb-2">
+        <div class="text-center font-thasadith text-base md:text-2xl -mt-3 mb-3">
           <span class="text-slate-300 font-semibold">รายการตามช่วงเวลาและรายการ</span>
         </div>
         <div class="card flex flex-wrap gap-2 md:gap-4">
-          <p-floatlabel variant="on">
-            <p-datePicker
-              [formControl]="selectedDates"
-              [iconDisplay]="'input'"
-              [showIcon]="true"
-              [readonlyInput]="true"
-              selectionMode="range" styleClass="w-[250px]"/>
-            <label for="on_label">วันเริ่มต้น-วันสิ้นสุด</label>
-          </p-floatlabel>
-          <p-iconfield>
-            <input
-              type="text"
-              pInputText
-              name="detail" placeholder="รายการ"
-              [formControl]="searchDetail"/>
-            <p-inputicon styleClass="pi pi-book"/>
-          </p-iconfield>
+          <div class="flex grow">
+            <p-floatlabel variant="on">
+              <p-datePicker
+                [formControl]="selectedDates"
+                [iconDisplay]="'input'"
+                [showIcon]="true"
+                [readonlyInput]="true"
+                selectionMode="range"
+                styleClass="w-[250px]"/>
+              <label for="on_label">วันเริ่มต้น-วันสิ้นสุด</label>
+            </p-floatlabel>
+          </div>
+          <div class="flex grow">
+            <p-floatlabel variant="on">
+              <p-iconfield>
+                <input
+                  type="text"
+                  pInputText
+                  [formControl]="searchDetail" class="w-[250px]"/>
+                <label for="on_label">รายการ</label>
+                <p-inputicon (click)="resetAll()"
+                             styleClass="pi pi-list cursor-pointer"/>
+              </p-iconfield>
+            </p-floatlabel>
+          </div>
         </div>
       </p-card>
     </div>
+    @if (account) {
+      <div class="table-container myCenter mt-3">
+        <p-table #tb
+                 [value]="account"
+                 [scrollable]="true"
+                 [rowHover]="true"
+                 scrollHeight="400px"
+                 [tableStyle]="{ 'min-width': '35vw' }"
+                 styleClass="p-datatable-striped"
+        >
+          <ng-template #caption>
+            <div class="flex items-center justify-between">
+              <span class="text-orange-400 text-2xl font-thasadith font-bold">
+                รายการของ: <span class="text-green-400">
+                {{ title }}
+              </span>
+              </span>
+            </div>
+          </ng-template>
+          <ng-template #header>
+            <tr>
+              <th>#</th>
+              <th style="min-width: 120px">
+                <div>วันที่</div>
+              </th>
+              <th style="min-width: 120px">
+                <div>จำนวนเงิน</div>
+              </th>
+              <th style="min-width: 150px">
+                <div>หมายเหตุ</div>
+              </th>
+              <th style="min-width: 150px">
+                <div>Action</div>
+              </th>
+            </tr>
+          </ng-template>
+          <ng-template #body let-account let-rowIndex="rowIndex">
+            <tr>
+              <td>{{ rowIndex + 1 }}</td>
+              <td>
+                {{ account.date | thaiDate }}
+              </td>
+              <td>
+                {{ account.amount | currency: '' : '' }}
+              </td>
+              <td>
+                {{ account.remark }}
+              </td>
+              <td>
+                @if (isAdmin()) {
+                  <i
+                    pTooltip="แก้ไข"
+                    (click)="showDialog(account)"
+                    tooltipPosition="bottom"
+                    class="pi pi-pen-to-square mx-2 text-sky-500"
+                  ></i>
+                  <p-confirmPopup/>
+                  <i
+                    pTooltip="ลบข้อมูล"
+                    (click)="confirm($event, account.id)"
+                    tooltipPosition="bottom"
+                    class="pi pi-trash text-red-400"
+                  ></i>
+                }
+              </td>
+            </tr>
+          </ng-template>
+          <ng-template #emptymessage>
+            <tr>
+              <td colspan="6">
+                <p-message
+                  severity="warn"
+                  icon="pi pi-exclamation-circle"
+                  text="ไม่พบข้อมูลรายจ่าย" styleClass="h-full"/>
+              </td>
+            </tr>
+          </ng-template>
+          <ng-template #summary>
+            <div
+              class="flex items-center justify-around font-bold leading-9 bg-gray-900"
+            >
+              <span>
+                รวม:
+                <span class="text-orange-400 mx-3">
+                  {{ account ? account.length : 0 }}
+                </span>
+                รายการ.
+              </span>
+              <span>
+                เป็นเงิน:
+                <span class="text-orange-400 mx-3">
+                  {{ getTotal() | currency: '' : '' }}
+                </span>
+                บาท
+              </span>
+            </div>
+          </ng-template>
+        </p-table>
+      </div>
+    }
 
   `,
   styles: ``
@@ -70,6 +180,10 @@ export class AccountBetweenDetailComponent implements OnInit {
   results$: Observable<any> = new Observable();
 
   ngOnInit(): void {
+    /**
+     * 1.เมื่อเลือกวันเริ่มต้น จะหน่วงเวลารอให้เลือกวันสิ้นสุด
+     * 2.เมื่อเลือกวันสิ้นสุดจะส่งไปประมวลผลใน resultDetails()
+     * */
     this.results$ = this.searchDetail.valueChanges.pipe(
       debounceTime(600),
       distinctUntilChanged(),
@@ -153,5 +267,18 @@ export class AccountBetweenDetailComponent implements OnInit {
       },
       reject: () => this.toastService.showInfo('Info Message', 'You have rejected')
     });
+  }
+
+  showDialog(account: any) {
+
+  }
+
+  getTotal() {
+    return this.totalExpenses.reduce((total, n) => total + Number(n.amount), 0);
+  }
+
+  resetAll(): void {
+    this.searchDetail.setValue('');
+    this.selectedDates.setValue('');
   }
 }
